6770a0a42acfa08e237c15ccb6cb0f04ac4cd78f
John Bender <jbender11@yahoo.com>
2020-04-07 16:14:52 -0500

    new file:   .classpath 	new file:   .ignore 	new file:   .project 	new file:   .settings/org.eclipse.jdt.core.prefs 	new file:   bin/application/.gitignore 	new file:   build.fxbuild 	new file:   src/application/Main.java 	new file:   src/application/application.css

---
+++ src/application/Main.java
@@ -0,0 +1,39 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			primaryStage.setTitle("Hello");
+			Button button  = new Button("Press Me!");
+			button.setOnAction(e -> System.out.println("Button clicked!!!!!!!!!!!!"));
+			button.setOnAction(e -> {
+				System.out.println("Hello World.");
+			});
+			StackPane layout = new StackPane();
+			layout.getChildren().add(button);
+					
+			
+//			BorderPane root = new BorderPane();
+			Scene scene = new Scene(layout,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
